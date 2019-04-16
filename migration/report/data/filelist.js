ae0857eabf6655b50ff3f2ec82145ff0be98f452
lordsap <jeffrey.lord@sap.com>
2019-04-16 18:58:41 +0000

    working

---
+++ migration/report/data/filelist.js
@@ -0,0 +1,485 @@
+var main_filelist = [
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\AccountingPostingStatus.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\AccountingPostingStatus.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 0
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\AccountingTransferStatus.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\AccountingTransferStatus.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 1
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentCategory.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentCategory.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 2
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentHeader.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentHeader.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 3
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentItem.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentItem.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 4
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentType.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\BillingDocumentType.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 5
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\CustomerBasicData.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\CustomerBasicData.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 6
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\CustomerGroup.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\CustomerGroup.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 7
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\DistributionChannel.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\DistributionChannel.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 8
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\KNA1_COM.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\KNA1_COM.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 9
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\ProductHierarchyNode.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\ProductHierarchyNode.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 10
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\SAPClient.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\SAPClient.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 11
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\SDDocHeaderBusinessPartner.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\SDDocHeaderBusinessPartner.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 12
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\SDDocItemBusinessPartner.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\SDDocItemBusinessPartner.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 13
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\SDDocumentCategory.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\SDDocumentCategory.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 14
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\SalesDistrict.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\SalesDistrict.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 15
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\SalesOrganization.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\SalesOrganization.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 16
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\UnitOfMeasureName.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\UnitOfMeasureName.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 17
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\VBPA_HDR.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\VBPA_HDR.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 18
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\VBPA_ITM.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\VBPA_ITM.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 19
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\VBRK_COM.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\VBRK_COM.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 20
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\VBRP_COM.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\VBRP_COM.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 21
+    },
+    {
+        "name": "db\\results\\sap_hba_ecc_2019415-151550\\db\\src\\VBUK_COM.hdbcalculationview",
+        "pkgOnlyName": "ults\\sap_hba_ecc_2019415-151550\\db\\src\\VBUK_COM.hdbcalculationview",
+        "ext": "hdbcalculationview",
+        "container": "db",
+        "index": 22
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\AccountingPostingStatus.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\AccountingPostingStatus.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 23
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\AccountingTransferStatus.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\AccountingTransferStatus.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 24
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentCategory.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentCategory.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 25
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentHeader.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentHeader.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 26
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentItem.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentItem.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 27
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentType.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentType.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 28
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\CustomerBasicData.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\CustomerBasicData.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 29
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\CustomerGroup.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\CustomerGroup.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 30
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\DistributionChannel.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\DistributionChannel.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 31
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\KNA1_COM.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\KNA1_COM.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 32
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\ProductHierarchyNode.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\ProductHierarchyNode.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 33
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SAPClient.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SAPClient.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 34
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SDDocHeaderBusinessPartner.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SDDocHeaderBusinessPartner.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 35
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SDDocItemBusinessPartner.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SDDocItemBusinessPartner.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 36
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SDDocumentCategory.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SDDocumentCategory.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 37
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SalesDistrict.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SalesDistrict.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 38
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SalesOrganization.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SalesOrganization.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 39
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\UnitOfMeasureName.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\UnitOfMeasureName.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 40
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBPA_HDR.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBPA_HDR.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 41
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBPA_ITM.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBPA_ITM.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 42
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBRK_COM.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBRK_COM.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 43
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBRP_COM.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBRP_COM.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 44
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBUK_COM.hdbcalculationview.properties",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBUK_COM.hdbcalculationview.properties",
+        "ext": "properties",
+        "container": "db",
+        "index": 45
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentCategory.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentCategory.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 46
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\AccountingPostingStatus.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\AccountingPostingStatus.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 47
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentHeader.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentHeader.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 48
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\AccountingTransferStatus.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\AccountingTransferStatus.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 49
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentItem.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentItem.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 50
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\BillingDocumentType.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\BillingDocumentType.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 51
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\CustomerBasicData.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\CustomerBasicData.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 52
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\CustomerGroup.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\CustomerGroup.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 53
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\DistributionChannel.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\DistributionChannel.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 54
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\KNA1_COM.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\KNA1_COM.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 55
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\ProductHierarchyNode.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\ProductHierarchyNode.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 56
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SDDocHeaderBusinessPartner.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SDDocHeaderBusinessPartner.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 57
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SAPClient.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SAPClient.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 58
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SDDocItemBusinessPartner.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SDDocItemBusinessPartner.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 59
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SDDocumentCategory.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SDDocumentCategory.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 60
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SalesOrganization.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SalesOrganization.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 61
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBRK_COM.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBRK_COM.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 62
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBPA_HDR.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBPA_HDR.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 63
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\SalesDistrict.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\SalesDistrict.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 64
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\UnitOfMeasureName.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\UnitOfMeasureName.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 65
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBPA_ITM.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBPA_ITM.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 66
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBRP_COM.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBRP_COM.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 67
+    },
+    {
+        "name": "db\\src\\sap\\hba\\ecc\\VBUK_COM.hdbcalculationview.tags",
+        "pkgOnlyName": "\\sap\\hba\\ecc\\VBUK_COM.hdbcalculationview.tags",
+        "ext": "tags",
+        "container": "db",
+        "index": 68
+    }
+];
